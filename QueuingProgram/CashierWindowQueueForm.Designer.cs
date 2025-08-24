e3f1b171fb847007798c7d03bdb5e4e20009dac2
zronowa17-maker <zronowa17@gmail.com>
2025-08-24 15:26:29 +0800

    add form and button listview

---
+++ QueuingProgram/CashierWindowQueueForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace QueuingProgram
+{
+    partial class CashierWindowQueueForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnRefresh = new Button();
+            btnNext = new Button();
+            listCashierQueue = new ListView();
+            SuspendLayout();
+            // 
+            // btnRefresh
+            // 
+            btnRefresh.BackColor = SystemColors.ActiveCaption;
+            btnRefresh.Font = new Font("Rockwell", 32.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnRefresh.Location = new Point(33, 12);
+            btnRefresh.Name = "btnRefresh";
+            btnRefresh.Size = new Size(219, 98);
+            btnRefresh.TabIndex = 0;
+            btnRefresh.Text = "Refresh";
+            btnRefresh.UseVisualStyleBackColor = false;
+            // 
+            // btnNext
+            // 
+            btnNext.BackColor = SystemColors.ActiveCaption;
+            btnNext.Font = new Font("Rockwell", 32.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnNext.Location = new Point(33, 116);
+            btnNext.Name = "btnNext";
+            btnNext.Size = new Size(219, 98);
+            btnNext.TabIndex = 1;
+            btnNext.Text = "Next";
+            btnNext.UseVisualStyleBackColor = false;
+            // 
+            // listCashierQueue
+            // 
+            listCashierQueue.Location = new Point(269, 13);
+            listCashierQueue.Name = "listCashierQueue";
+            listCashierQueue.Size = new Size(498, 425);
+            listCashierQueue.TabIndex = 2;
+            listCashierQueue.UseCompatibleStateImageBehavior = false;
+            // 
+            // CashierWindowQueueForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.AppWorkspace;
+            ClientSize = new Size(800, 450);
+            Controls.Add(listCashierQueue);
+            Controls.Add(btnNext);
+            Controls.Add(btnRefresh);
+            Name = "CashierWindowQueueForm";
+            Text = "CashierWindowQueueForm";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnRefresh;
+        private Button btnNext;
+        private ListView listCashierQueue;
+    }
+}
