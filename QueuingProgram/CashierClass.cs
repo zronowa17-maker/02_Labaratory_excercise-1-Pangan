72ff5c5dc20803ad4f2caf1dc15289ab71009561
zronowa17-maker <zronowa17@gmail.com>
2025-08-24 14:52:56 +0800

    create class and add codes

---
+++ QueuingProgram/CashierClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueuingProgram
+{
+    public class CashierClass
+    {
+        
+        private static int x = 10000;
+        public static string getNumberInQueue = "";
+        public static Queue<string> CashierQueue = new Queue<string>();
+
+        public static string CashierGeneratedNumber(string CashierNumber)
+        {
+            x++; 
+            CashierNumber = CashierNumber + x.ToString();
+            return CashierNumber;
+        }
+    }
+}
+
+
+
+  
+       
